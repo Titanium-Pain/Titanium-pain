40d39caaf051bfc1dc2c6aa2c1ac8306a5b28091
ddddwing <d_e_ddwing@hotmail.com>
2016-06-01 00:52:42 +0800

    player can move now

---
+++ assets/Script/Player.js
@@ -0,0 +1,96 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+        speed:0,
+        /*moveup:false,
+        movedown:false,
+        moveleft:false,
+        moveright:false,*/
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        var moveup=false;
+        var movedown=false;
+        var moveleft=false;
+        var moveright=false;
+        this.setInputControl();
+    },
+    
+    setInputControl: function () {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.w:
+                        //self.moveleft =false;
+                        //self.moveright= false;
+                        self.moveup = true;
+                        //self.movedown = false;
+                        break;
+                    case cc.KEY.s:
+                        //self.moveleft =false;
+                        //self.moveright= false;
+                        //self.moveup = false;
+                        self.movedown = true;
+                        break;
+                    case cc.KEY.a:
+                        self.moveleft =true;
+                        //self.moveright= false;
+                        //self.moveup = false;
+                        //self.movedown = false;
+                        break;
+                    case cc.KEY.d:
+                        //self.moveleft =false;
+                        self.moveright= true;
+                        //self.moveup = false;
+                        //self.movedown = false;
+                        break;
+                }
+            },
+            onKeyReleased: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.w:
+                        self.moveup = false;
+                        break;
+                    case cc.KEY.s:
+                        self.movedown = false;
+                        break;
+                    case cc.KEY.a:
+                        self.moveleft = false;
+                        break;
+                    case cc.KEY.d:
+                        self.moveright = false;
+                        break;
+                }
+            }
+        }, self.node);
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    update: function (dt) {
+        if(this.moveleft){
+            this.node.x-=this.speed;
+        }
+        if(this.moveright){
+            this.node.x+=this.speed;
+        }
+        if(this.movedown){
+            this.node.y-=this.speed;
+        }
+        if(this.moveup){
+            this.node.y+=this.speed;
+        }
+    },
+});
---
+++ library/imports/eb/eb354efa-17a0-44be-94f3-5426374f0dfd.js
@@ -0,0 +1,96 @@
+cc.Class({
+    "extends": cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+        speed: 0
+    },
+
+    /*moveup:false,
+    movedown:false,
+    moveleft:false,
+    moveright:false,*/
+    // use this for initialization
+    onLoad: function onLoad() {
+        var moveup = false;
+        var movedown = false;
+        var moveleft = false;
+        var moveright = false;
+        this.setInputControl();
+    },
+
+    setInputControl: function setInputControl() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function onKeyPressed(keyCode, event) {
+                switch (keyCode) {
+                    case cc.KEY.w:
+                        //self.moveleft =false;
+                        //self.moveright= false;
+                        self.moveup = true;
+                        //self.movedown = false;
+                        break;
+                    case cc.KEY.s:
+                        //self.moveleft =false;
+                        //self.moveright= false;
+                        //self.moveup = false;
+                        self.movedown = true;
+                        break;
+                    case cc.KEY.a:
+                        self.moveleft = true;
+                        //self.moveright= false;
+                        //self.moveup = false;
+                        //self.movedown = false;
+                        break;
+                    case cc.KEY.d:
+                        //self.moveleft =false;
+                        self.moveright = true;
+                        //self.moveup = false;
+                        //self.movedown = false;
+                        break;
+                }
+            },
+            onKeyReleased: function onKeyReleased(keyCode, event) {
+                switch (keyCode) {
+                    case cc.KEY.w:
+                        self.moveup = false;
+                        break;
+                    case cc.KEY.s:
+                        self.movedown = false;
+                        break;
+                    case cc.KEY.a:
+                        self.moveleft = false;
+                        break;
+                    case cc.KEY.d:
+                        self.moveright = false;
+                        break;
+                }
+            }
+        }, self.node);
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    update: function update(dt) {
+        if (this.moveleft) {
+            this.node.x -= this.speed;
+        }
+        if (this.moveright) {
+            this.node.x += this.speed;
+        }
+        if (this.movedown) {
+            this.node.y -= this.speed;
+        }
+        if (this.moveup) {
+            this.node.y += this.speed;
+        }
+    }
+});
